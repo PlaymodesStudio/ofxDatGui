ca204f211f738f9e99e2d24f1cae5ed092f77000
Eduard Frigola Bagué <eduardfrigola@yahoo.com>
2018-05-14 16:38:14 +0200

    fix scrollview button position

--- src/components/ofxDatGuiScrollView.h
+++ src/components/ofxDatGuiScrollView.h
@@ -31,6 +31,7 @@ class ofxDatGuiScrollView : public ofxDatGuiComponent {
         {
             mAutoHeight = true;
             mNumVisible = nVisible;
+            mY = 0;
             setTheme(ofxDatGuiComponent::getTheme());
             ofAddListener(ofEvents().mouseScrolled, this, &ofxDatGuiScrollView::onMouseScrolled, OF_EVENT_ORDER_BEFORE_APP);
         }
